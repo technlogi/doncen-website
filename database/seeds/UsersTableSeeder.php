46168607653aced076e1e4be7484e3af0050d0d2
Technlogi <technlogi@gmail.com>
2018-03-13 16:38:39 +0530

    Initiate Doncen 13-03-2018

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456'),
+            'created_at' =>new \DateTime()	,
+            'updated_at' => new \DateTime()	
+        ]);
+         
+    }
+}
