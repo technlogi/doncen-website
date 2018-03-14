4a90caf7481512f18617e2cd7ee400f2e8ebe16c
Technlogi <technlogi@gmail.com>
2018-03-14 13:29:42 +0530

    add relationship between category,subcategory and specification

--- app/Models/Specification.php
+++ app/Models/Specification.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specification extends Model
 {
-    //
+    public function subcategory()
+    {
+        return $this->belongsTo('App\Models\Subcategory');
+    }
 }
--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subcategory extends Model
 {
-    //
+    public function specifications()
+    {
+        return $this->hasMany('App\Models\Specification');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
