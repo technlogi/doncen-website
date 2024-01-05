2fc3e590f145d62c1bbf2aab035a6e2099d9f5e3
technlogi <technlogi@technlogi.com>
2024-01-05 17:51:03 +0530

    change env to dev

--- app/Http/Middleware/ActivityLog.php
+++ app/Http/Middleware/ActivityLog.php
@@ -21,8 +21,7 @@ class ActivityLog
 	public function handle($request, Closure $next)
 	{
 
-		echo "YES";
-		dd();
+		
 		function isMobileDevice() {
 			    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
 			|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
